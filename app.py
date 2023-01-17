1188c1726b9d15442d3ea1fa308643862c0be2e3
Thomas Fercher <thomas.fercher@outlook.com>
2023-01-17 14:24:10 +0100

    Api Key

--- app.py
+++ app.py
@@ -40,8 +40,6 @@ def recipe():
 
     return Response(result, mimetype="application/json")
 
-
-apiKey = "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"
 baseUrl = "https://api.openai.com/v1/completions"
 model = "text-davinci-003"
 temperature = 0.5
