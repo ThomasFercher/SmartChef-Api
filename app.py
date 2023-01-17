248deb9bb7c63d69cbf4a19cce49ddeb686f1204
Thomas Fercher <thomas.fercher@outlook.com>
2023-01-17 15:03:03 +0100

    Improve Response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import requests
 
 
@@ -10,7 +10,7 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/recipe", methods=["POST"])
+@app.route("/recipe", methods=["POST"], )
 def recipe():
     request_body = request.get_json()
     
@@ -22,22 +22,27 @@ def recipe():
 
     prompt = create_prompt(ingredients, tools)
 
-    print(prompt)
+    
+    result = request_recipe(prompt)
 
-    return request_recipe(prompt)
+    if(result == None):
+        return "Error Fetching Response", 500
+
+    return Response(result, mimetype="application/json")
 
 
-apiKey = "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"
+apiKey = "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"
 baseUrl = "https://api.openai.com/v1/completions"
 model = "text-davinci-003"
-temperature = 1
-max_tokens = 1000
+temperature = 0.5
+max_tokens = 500
 templatePrompt = """
 Create a Cooking recipe with the available Ingredients using the available Tools.
 Explain every step precisly.
 Add Measurements units to the Ingridient list.
 Use both Celcius and Fahrenheit.
 Output only JSON using the following Format: {\"title\": \"title of recipe\",\"ingridients\": [\"ingridient1\", \"ingridient2\"], \"tools\": [\"tool1\", \"tool2\"] ,\"steps\" : [  \"1\": \"step1 description\", \"2\": \"step2 description\"], \"sidenotes\": \"notes\"}
+Format the Output correct
 """
 
 
@@ -60,25 +65,30 @@ def request_recipe(prompt: str):
     }
 
     response = requests.post(baseUrl, json=data, headers=headers, timeout=20)
-    print(response.status_code)
-    if(response.status_code != 200):
-        print("Error: ", response.status_code)
-        return None
-
-
-
+  
     json = response.json() 
 
-    print(json)
+    if(response.status_code != 200):
+        print("Error: ", response.status_code)
+        print(json["error"])
+        
+        return None
 
     id = json["id"]
-    
     choice = json["choices"][0]
-
     finish_reason = choice["finish_reason"]
-    answer = choice["text"]
+    answer: str = choice["text"] 
+    jsonStart =answer.find("{")
+    answer = answer[jsonStart:]
+
+
+
 
     print("id: ", id)
     print("finish_reason: ", finish_reason)
 
     return answer
+
+
+
+
