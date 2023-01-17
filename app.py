9f32d00e1e01fae7c43f45b4792d187d6e79e7da
Thomas Fercher <thomas.fercher@outlook.com>
2023-01-17 16:37:44 +0100

    Improvement

--- app.py
+++ app.py
@@ -10,46 +10,68 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+
+
+
 @app.route("/recipe", methods=["POST"], )
 def recipe():
-    request_body = request.get_json()
-    
-    ingredients = request_body["ingredients"]
-    tools = request_body["tools"]
+    request_body:dict = request.get_json()
+
+
+
+    ingredients = request_body.get("ingredients")
+    tools =  request_body.get("tools")
+    servingAmount = request_body.get("servingAmount")
+    targetCalories = request_body.get("targetCalories")
 
     print(ingredients)
     print(tools)
+    print(servingAmount)
+    print(targetCalories)
 
-    prompt = create_prompt(ingredients, tools)
+    prompt = create_prompt(ingredients, tools, servingAmount, targetCalories)
 
+    print(prompt)
     
     result = request_recipe(prompt)
 
     if(result == None):
-        return "Error Fetching Response", 500
+        return Response( "Error Fetching Response", 500,mimetype="application/json")
 
     return Response(result, mimetype="application/json")
 
 
-apiKey = "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"
+apiKey = "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"
 baseUrl = "https://api.openai.com/v1/completions"
 model = "text-davinci-003"
 temperature = 0.5
-max_tokens = 500
+max_tokens = 1000
 templatePrompt = """
-Create a Cooking recipe with the available Ingredients using the available Tools.
-Explain every step precisly.
+Create a Cooking recipe with the available Ingredients using the available Tools. Respect the Serving Amount.
+Explain every step precisly. 
+Tell me the calories of each ingridient.
 Add Measurements units to the Ingridient list.
-Use both Celcius and Fahrenheit.
-Output only JSON using the following Format: {\"title\": \"title of recipe\",\"ingridients\": [\"ingridient1\", \"ingridient2\"], \"tools\": [\"tool1\", \"tool2\"] ,\"steps\" : [  \"1\": \"step1 description\", \"2\": \"step2 description\"], \"sidenotes\": \"notes\"}
+Use both Celcius and Fahrenheit
+Output in the following Format: 
+{"title": str,
+"servingAmount: int,
+"ingridients": [{"name": str, "amount": str, "kcal": int}], 
+"tools": [""], 
+"steps" : [{"number": int, "description": str}], 
+"optionalSteps" : [{"number": int, "description": str}],
+}
 Format the Output correct
 """
 
 
-def create_prompt(ingredients: list, tools: list):
-   
+def create_prompt(ingredients: list, tools: list, servingAmount: int = None, targetCalories: int = None):
+    prompt = f"{templatePrompt}Available Ingridients: {', '.join(ingredients)} \nAvailable Tools: {', '.join(tools)}"
+    if(servingAmount != None):
+        prompt += f" \nServing Amount: {servingAmount}"
+    #if(targetCalories != None):
+    #    prompt += f" \Calories: {targetCalories}"
 
-    return f"{templatePrompt} \nAvailable Ingridients: {', '.join(ingredients)} \nAvailable Tools: {', '.join(tools)}"
+    return prompt
 
 def request_recipe(prompt: str):
 
@@ -58,13 +80,16 @@ def request_recipe(prompt: str):
         "temperature": temperature,
         "max_tokens": max_tokens,
         "prompt": prompt,
+        "presence_penalty": 0.0, # -2.0 to 2.0
+        "frequency_penalty": 0.0, # -2.0 to 2.0
+        "stream": False,
     }
     headers = {
         "Content-type": "application/json",
         "Authorization": "Bearer " + apiKey,
     }
 
-    response = requests.post(baseUrl, json=data, headers=headers, timeout=20)
+    response = requests.post(baseUrl, json=data, headers=headers, timeout=30)
   
     json = response.json() 
 
